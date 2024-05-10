61976bdc3c4d325fcb8790a74208e59e4ebc1eff
ethanG <ethangunzb@gmail.com>
2024-05-11 00:35:07 +0200

    page acceuile

---
+++ app/src/main/java/com/openaidandroidfeatures/WareHouses.kt
@@ -0,0 +1,11 @@
+package com.openaidandroidfeatures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WareHouses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ware_houses)
+    }
+}
